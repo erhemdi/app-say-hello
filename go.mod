8a381a640915287f4005082baf5dafb0223100bb
mehre <mehreartupid@gmail.com>
2021-02-16 07:41:17 +0700

    add app

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mehre/app-say-hello
+
+go 1.15
+
+require github.com/mehre/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	say_hello "github.com/mehre/go-say-hello"
+)
+
+func main() {
+	fmt.Println(say_hello.SayHello())
+}
