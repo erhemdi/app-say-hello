1137b8cd75c6b67bdd53536a08e8a68aaab62296
mehre <mehreartupid@gmail.com>
2021-02-16 07:56:57 +0700

    update app - major version

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/mehre/app-say-hello
 
 go 1.15
 
-require github.com/mehre/go-say-hello v1.5.0
+require github.com/mehre/go-say-hello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	say_hello "github.com/mehre/go-say-hello"
+	say_hello "github.com/mehre/go-say-hello/v2"
 )
 
 func main() {
-	fmt.Println(say_hello.SayHello())
+	fmt.Println(say_hello.SayHello("Golang"))
 }
